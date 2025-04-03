a8fc8c7108f89cc6cc839a1c3c242b10bed18da9
Kshitij0O7 <kshitij.mahajan.20042@iitgoa.ac.in>
2025-04-03 15:07:46 +0530

    Speed Testing

--- consumer.js
+++ consumer.js
@@ -1,7 +1,8 @@
 const { Kafka } = require('kafkajs');
 const protobuf = require('protobufjs');
 const bs58 = require('bs58');
-// const path = require('path');
+const { createClient } = require('@supabase/supabase-js');
+const { writeToExcel } = require('./writeXl.js');
 const { CompressionTypes, CompressionCodecs } = require("kafkajs");
 const LZ4 = require("kafkajs-lz4");
 require('dotenv').config();
@@ -12,6 +13,10 @@ let ParsedIdlBlockMessage;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const topic = 'bsc.dextrades.proto';
+
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.PUBLIC_URL, process.env.API_KEY)
 
 const loadProto = async () => {
     const root = await protobuf.load('proto/evm/dex_block_message.proto');
@@ -59,7 +64,16 @@ const kafka = new Kafka({
     }
 });
 
+const FILE_PATH = 'output.xlsx';
+const SHEET_NAME = 'Messages';
+
+const writeQueue = [];
+const write =  () => writeToExcel(FILE_PATH, SHEET_NAME, writeQueue);
+// Run file writing at intervals to prevent congestion
+setInterval(write, 1000); // Every 1 second
+
 const consumer = kafka.consumer({ groupId: username + '-group123' });
+// let id = 0;
 
 async function run() {
     await loadProto(); // Load proto before starting Kafka
@@ -70,11 +84,15 @@ async function run() {
         autoCommit: false,
         eachMessage: async ({ partition, message }) => {
             try {
+                let time = Date.now();
                 const buffer = message.value;
                 const decoded = ParsedIdlBlockMessage.decode(buffer);
                 const msgObj = ParsedIdlBlockMessage.toObject(decoded, { bytes: Buffer });
-                console.log("\nNew Protobuf Message Received:\n");
-                printProtobufMessage(msgObj);
+                let timeStamp = msgObj.Header.Time.low*1000;
+                let hash = convertBytes(msgObj.Header.TxHash);
+                writeQueue.push([hash, time, timeStamp]);
+                // writeToExcel(FILE_PATH, SHEET_NAME, [hash, time, timeStamp]);
+                console.log("Entry Sent");
             } catch (err) {
                 console.error('Error decoding Protobuf message:', err);
             }
@@ -82,4 +100,25 @@ async function run() {
     });
 }
 
+const fetch = async () => {
+    try {
+        const { data } = await supabase.from('proto').select();
+        // console.log(data);
+        for(num in data){
+            let msg = data[num]
+            let trades = msg.message.Trades
+            for(i in trades){
+                let time = trades[i];
+                console.log(time);
+                // console.log(convertBytes(hash), "end");
+            }
+            // hash = convertBytes(hash);
+            // console.log(hash);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 run().catch(console.error);
+// fetch();
